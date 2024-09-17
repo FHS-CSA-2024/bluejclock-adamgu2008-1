921883fa1ee5560f90282d8797a7a643b34d1a80
adamgu2008 <adamgu2008@gmail.com>
2024-09-17 10:10:15 -0700

    Update ClockDriver.java

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,3 +1,5 @@
+package src.main.java;
+
 
 public class ClockDriver
 {
@@ -15,6 +17,25 @@ public class ClockDriver
         //  * Tick test for 09:59 to 10:00
         //  * Tick test for 23:59 to 00:00
         
+        System.out.println("Testing Clock Display: ");
+        ClockDisplay test1 = new ClockDisplay();
+        String test1Output = test1.getTime();
+        System.out.println("\tEmpty Con. Test - " + test1Output);
+        
+        ClockDisplay test2 = new ClockDisplay(2, 45);
+        String test2Output = test2.getTime();
+        System.out.println("\tArg Con. Test - "+ test2Output);
+        
+        test2.setTime(5, 15);
+        String test3Output = test2.getTime();
+        System.out.println("\tSet Time Test - "+ test3Output);
+        
+        ClockDisplay tickTest1 = new ClockDisplay(3, 32);
+        String tickTest1Output = tickTest1.getTime();
+        System.out.println("\tTickTest1 Before increment - " + tickTest1Output);
+        tickTest1.timeTick();
+        tickTest1Output = tickTest1.getTime();
+        System.out.println("\tTickTest1 after increment - " + tickTest1Output);
         
         //Implement tests for your ClockDisplaySeconds
         //  * Test that you can do the basics
@@ -27,6 +48,27 @@ public class ClockDriver
         //  * Tick test for 01:00:59 to 01:01:00
         //  * Tick test for 01:59:59 to 02:00:00
         //  * Tick test for 23:59:59 to 00:00:00
+        System.out.println("Testing Clock Display Seconds: ");
+        ClockDisplaySeconds testA = new ClockDisplaySeconds();
+        String testAOutput = testA.getTimeSec();
+        System.out.println("\tEmpty Con. Test - " + test1Output);
+        
+        ClockDisplaySeconds testB = new ClockDisplaySeconds(2, 45, 39);
+        String testBOutput = testB.getTimeSec();
+        System.out.println("\tArg Con. Test - "+ test2Output);
+        
+        testB.setTime(5, 15, 12);
+        String testCOutput = testB.getTimeSec();
+        System.out.println("\tSet Time Test - "+ test3Output);
+        
+        ClockDisplaySeconds tickTestA = new ClockDisplaySeconds(3, 32, 59);
+        String tickTestAOutput = tickTestA.getTimeSec();
+        System.out.println("\tTickTest1 Before increment - " + tickTestAOutput);
+        tickTestA.timeTick();
+        tickTestAOutput = tickTestA.getTimeSec();
+        System.out.println("\tTickTest1 after increment - " + tickTestAOutput);
+        
+        
         
         /*CHALLENGE*/
         //Implement tests for your ClockDisplay12Hour
